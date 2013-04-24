c7d4a6974a19152b3e713aeaf8068f7ab36f4969
Alex Bertram <alex@bedatadriven.com>
2013-04-24 13:49:48 +0200

    Fixed error in htm fetcher encountered on first run

--- bin/create-gtfs-htm.sh
+++ bin/create-gtfs-htm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATE=$(date +'%Y%m%d')
-wget ../kv1feeds/htm -N --accept=zip -q -P ../kv1feeds/htm -nd -r http://kv1.openov.nl/htm/ -l 1
+mkdir -p ../kv1feeds/arriva
+wget ../kv1feeds/arriva -N --accept=zip -P ../kv1feeds/arriva -nd -r http://kv1.openov.nl/arriva/ -l 1
 python manager.py -d kv1htm -f ../kv1feeds/htm
 rm -rf /tmp/*.txt
 psql -d kv1htm -f ../sql/gtfs-shapes-htm.sql
 psql -d kv1htm -f ../sql/gtfs-shapes-passtimes.sql
+mkdir -p ../gtfs/htm
 zip -j ../gtfs/htm/gtfs-kv1htm-$DATE.zip /tmp/*.txt
 rm ../gtfs/htm/gtfs-kv1htm-latest.zip
 ln -s gtfs-kv1htm-$DATE.zip ../gtfs/htm/gtfs-kv1htm-latest.zip
